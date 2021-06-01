5a695b03b4471f342b7f26ff8c272e4766203b0b
peterkollerlv <peterkollerlv@live.com>
2021-06-01 11:53:45 +0200

    replaced folders

--- public-api.ts
+++
@@ -1,5 +0,0 @@
-import { ButtonExample, ButtonExampleProps } from './src/molitio-ui/core/examples';
-import { PagedMenuComponent, PagedMenuItemComponent } from './src/component/PagedMenu';
-import { MenuItem } from './src/interface/MenuItem';
-
-export { ButtonExample, ButtonExampleProps, PagedMenuComponent, PagedMenuItemComponent, MenuItem };
