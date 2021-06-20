beff1eb0434c5981f45ee6bec9e398c757240b05
peterkollerlv <peterkollerlv@live.com>
2021-06-20 08:46:34 +0200

    rollup config initialization

--- rollup.config.cjs
+++
@@ -1,9 +0,0 @@
-const babel = () => require('@rollup/plugin-babel');
-module.exports = {
-    input: 'src/index.ts',
-    output: {
-        dir: 'lib',
-        format: 'cjs',
-    },
-    plugins: [babel({ babelHelpers: 'bundled' })],
-};
