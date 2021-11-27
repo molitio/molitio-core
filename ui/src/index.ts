4f8df8f38555cfcb9c76431316374176d36c86d9
Peter Koller <peterkollerlv@live.com>
2021-11-27 20:51:16 +0100

    correction to index

--- ui/src/index.ts
+++ ui/src/index.ts
@@ -1,17 +1,10 @@
-import {
-  PagedMenuComponent,
-  PagedMenuItemComponent,
-} from "ui-paged-menu/component/PagedMenu";
-import {
-  MenuItem,
-  MenuPageData,
-  MenuPageDataCollection,
-} from "ui-paged-menu/component/PagedMenu/interface/IPagedMenu";
+import { UiNavMenuComponent } from './ui-nav-menu/component/UiNavMenuComponent';
+import { UiNavMenuItemComponent } from './ui-nav-menu/component/UiNavMenuItemComponent';
+import { UiNavMenuItem, UiNavMenuPageData, UiNavMenuPageDataCollection } from 'ui-nav-menu/interface/IUiNavMenu';
 
-export {
-  PagedMenuComponent,
-  PagedMenuItemComponent,
-  MenuItem,
-  MenuPageData,
-  MenuPageDataCollection,
-};
+/* const number1: number = 3;
+export const number1: number;  */
+
+export { UiNavMenuComponent, UiNavMenuItemComponent };
+
+export type { UiNavMenuItem, UiNavMenuPageData, UiNavMenuPageDataCollection };
