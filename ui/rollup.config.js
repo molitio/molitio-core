d36deffeed704354015b1224c207c56b9677fd08
Peter Koller <peterkollerlv@live.com>
2021-11-28 11:21:55 +0100

    working storybook and rollup

--- ui/rollup.config.js
+++ ui/rollup.config.js
@@ -3,6 +3,8 @@ import typescript from '@rollup/plugin-typescript';
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import buble from 'rollup-plugin-buble';
+import sizes from 'rollup-plugin-sizes';
 
 const packageJson = require('./package.json');
 
@@ -17,53 +19,48 @@ const globals = {
 
 export default {
     input: 'src/index.ts',
-    plugins: [typescript(), peerDepsExternal(), resolve(), commonjs(), peerDepsExternal()],
+    plugins: [
+        typescript(),
+        peerDepsExternal(),
+        resolve(),
+        commonjs(),
+        peerDepsExternal(),
+        buble(),
+        sizes(),
+        babel({
+            extensions,
+            babelHelpers: 'bundled',
+            include: ['src/**/*'],
+            exclude: 'node_modules/**',
+        }),
+    ],
     output: [
         //unbundled esm
         {
+            name: '@molitio-core/ui',
             file: packageJson.module,
             format: 'esm',
             globals,
             sourcemap: true,
-            plugins: [
-                resolve({ extensions }),
-                babel({
-                    extensions,
-                    include: ['src/**/*'],
-                    exclude: 'node_modules/**',
-                }),
-            ],
+            plugins: [],
         },
         //bundled esm
         {
+            name: '@molitio-core/ui',
             file: packageJson.bundle.esm,
             format: 'esm',
             globals,
             sourcemap: true,
-            plugins: [
-                resolve({ extensions }),
-                babel({
-                    extensions,
-                    babelHelpers: 'bundled',
-                    include: ['src/**/*'],
-                    exclude: 'node_modules/**',
-                }),
-            ],
+            plugins: [],
         },
         //unbundled cjs
         {
+            name: '@molitio-core/ui',
             file: packageJson.main,
             format: 'cjs',
             globals,
             sourcemap: true,
-            plugins: [
-                resolve({ extensions }),
-                babel({
-                    extensions,
-                    include: ['src/**/*'],
-                    exclude: 'node_modules/**',
-                }),
-            ],
+            plugins: [],
         },
         //bundled umd
         {
@@ -72,15 +69,7 @@ export default {
             format: 'umd',
             globals,
             sourcemap: true,
-            plugins: [
-                resolve({ extensions }),
-                babel({
-                    extensions,
-                    babelHelpers: 'bundled',
-                    include: ['src/**/*'],
-                    exclude: 'node_modules/**',
-                }),
-            ],
+            plugins: [],
         },
     ],
 
