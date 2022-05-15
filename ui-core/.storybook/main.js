908e5e1d5f28a9f950986e16c049d7ff8e904e8c
Peter Koller <peterkollerlv@live.com>
2022-05-15 08:29:54 +0200

    fix(*): set css loader to module

--- ui-core/.storybook/main.js
+++ ui-core/.storybook/main.js
@@ -4,13 +4,40 @@ const path = require('path');
 module.exports = {
     stories: ['../src/**/*.stories.*'],
     //TODO: resolve issues with: '@storybook/addon-a11y
-    addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/theming', '@storybook/preset-scss'],
+    addons: [
+        '@storybook/addon-links',
+        '@storybook/addon-essentials',
+        '@storybook/theming',
+     /*    {
+            name: 'storybook-addon-sass-postcss',
+            options: {
+                postcssLoaderOptions: {
+                    implementation: require('postcss'),
+                },
+            },
+        }, */
+        {
+            name: '@storybook/preset-scss',
+            options: {
+                cssLoaderOptions: {
+                    modules: true,
+                   // localIdentName: '[name]__[local]--[hash:base64:5]',
+                },
+            },
+        },
+    ],
     webpackFinal: async (config) => {
         if (!config.resolve) {
             config.resolve = {};
         }
 
         config.resolve.plugins = [...(config.resolve.plugins || []), new TsconfigPathsPlugin()];
+
+        /*      config.module.rules.push({
+            test: /\.scss$/,
+            use: ['style-loader', 'css-loader', 'sass-loader'],
+            include: path.resolve(__dirname, '../'),
+        }); */
 
         return config;
     },
