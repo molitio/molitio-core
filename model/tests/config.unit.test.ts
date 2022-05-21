88c513e18150a116ed92b910a1285a8bfdb06b91
Peter Koller <peterkollerlv@live.com>
2022-05-21 09:46:41 +0200

    57 configure semver for versioning and create release branch for publish (#62)
    * chore(*): structure refactor
    
    * chore(*): moved resource api

--- model/tests/config.unit.test.ts
+++
@@ -1,9 +0,0 @@
-/* import jasmine  from 'jasmine';
-import { apiConfig } from '../src/config/apiConfig'
-
-
-describe('app config can read environment name', () => {
-  it('checking sampe test', () => {
-    expect(apiConfig.apiEndpoint).toEqual("molitio");
-  })
-}) */
