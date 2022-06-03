a8351dabecc6dca405b223755fcddf87b593f7f6
Peter Koller <peterkollerlv@live.com>
2022-06-03 16:12:18 +0200

    chore: storybook jest pre config

--- ui-core/.storybook/main.js
+++ ui-core/.storybook/main.js
@@ -8,6 +8,7 @@ module.exports = {
         '@storybook/addon-essentials',
         '@storybook/addon-interactions',
         '@storybook/preset-scss',
+        '@storybook/addon-jest',
         // module
         /*         {
             name: `@storybook/preset-scss`,
